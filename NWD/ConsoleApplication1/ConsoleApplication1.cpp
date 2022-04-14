beddecf1ed46b8b8b69a9858259c543b0943d2ce
Duke-net <dukenet.ovh@gmail.com>
2022-04-14 14:03:37 +0200

    nwd nww

---
+++ NWD/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int NWD(int a, int b) {
+	while (a != b) {
+		if (a > b) {
+			a -= b;
+		}
+		else
+			b -= a;
+	}
+	return a;
+}
+
+int main() {
+	clock_t t;
+	t = clock();
+
+	int a, b;
+	cout << "podaj dwie liczby: ";
+	cin >> a >> b;
+
+	cout << "NWD: " << NWD(a, b) << endl;
+	cout << "NWW: " << NWD(a, b) / (a * b) << "\n\n";
+
+	cout << clock() - t << "milisekundy";
+}
